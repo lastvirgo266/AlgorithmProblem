8eba31d52b41275788109b0f83241a5ecdb8d09a
ShootedBall <wjdtmdwls45@gmail.com>
2018-04-13 20:43:59 +0900

    Change data type and add STL pair and vector

--- Baekjoon/1036.cpp
+++ Baekjoon/1036.cpp
@@ -1,49 +1,59 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<math.h>
 #include<memory.h>
+#include<utility>
+#include<vector>
+#include<algorithm>
 
+using namespace std;
 
-int number_count[37];
+// <size, digit>
+vector<pair<int, int>> number_count;
 
 int main(){
 
 
-	char tNumber[50] = { NULL };
 	char allNumber[50][50];
 
 	int N;
 	int digit_count = 0;
 
 	//Array set same value
-	memset(tNumber, NULL, sizeof(tNumber));
-	memset(number_count, 0, sizeof(number_count));
 	memset(*allNumber, NULL, sizeof(*allNumber));
+
+	for (int i = 0; i < 36; i++)
+		number_count.push_back(make_pair(0, i));
 
 
 
 	scanf("%d", &N);
-	scanf("%s", tNumber);
+
+	for (int k = 0; k < N; k++)
+		scanf("%s", allNumber[k]);
 
 
 	for (int k = 0; k < N; k++){
 
-
-		for (int i = 0; tNumber[i] != NULL; i++){
+		// Count digit
+		for (int i = 0; allNumber[k][i] != NULL; i++)
 			digit_count++;
-			allNumber[k][i] = tNumber[i];
-		}
+		
 
-
-
+		// Count Big digit
 		for (int i = 0; i < digit_count; i++){
 
 			// Count 0 to 9
-			if ((int)tNumber[i] < 58)
-				number_count[(int)tNumber[i] - 48] = pow((double)35, (digit_count - i));
+			if ((int)allNumber[k][i] < 58)
+				number_count[(int)allNumber[k][i] - 48].first = number_count[(int)allNumber[k][i] - 48].first + pow((double)35, (digit_count - i));
 
 			else
-				number_count[(int)tNumber[i] - 55] = pow((double)35, (digit_count - i));
+				number_count[(int)allNumber[k][i] - 55].first = number_count[(int)allNumber[k][i] - 55].first + pow((double)35, (digit_count - i));
+		}
+
+
+		// Test Print //
+		for (int i = 0; i < 36; i++){
+			printf("%d   %d\n", number_count[i].first, number_count[i].second);
 		}
 
 
@@ -53,10 +63,27 @@ int main(){
 		digit_count = 0;
 	}
 
+	sort(number_count.begin(), number_count.end());
+
+
+	// Test Print //
+	for (int i = 0; i < 36; i++){
+		printf("%d   %d\n", number_count[i].first, number_count[i].second);
+	}
+	
+
 
 	int K;
 
 	scanf("%d", &K);
+
+	int change_list[5];
+
+
+	for (int i = 0; i < K; i++)
+		change_list[i] = number_count[35 - i].second;
+
+		//Test Print //
 
 
 
