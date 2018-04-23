41ca23f730f18d6c4ad278808c3ca47745c05634
ShootedBall <wjdtmdwls45@gmail.com>
2018-04-23 14:32:19 +0900

    [15353] complete

--- Baekjoon/15353.cpp
+++ Baekjoon/15353.cpp
@@ -1,52 +1,102 @@
 #include<iostream>
-#include<vector>
 #define ASCII 48
 #define MAX 10
 using namespace std;
 
 inline void char_to_IntAdd(int *total, char *number){ *number == NULL ? *total : *total += ((int)*number - ASCII); }
 
-void DigitCount(int *number)
-{
-
-
-}
-
-
 int main(){
-	char A[10000]{};
-	char B[10000]{};
-	int total[100000]{0};
+	char A[10001]{};
+	char B[10001]{};
 
+	int A_max_digit = -1;
+	int B_max_digit = -1;
+	int A_B_max_digit;
+
+	int total[100000]{NULL};
+
+	// Input Line //
 	cin >> A;
 	cin >> B;
 
 	int i = 0;
 
+	// Compute max Digit Line//
 	while (A[i] || B[i])
 	{
-		char_to_IntAdd(total+i, A+i);
-		char_to_IntAdd(total+i, B+i);
+		if (A[i])
+			A_max_digit++;
+		
+		if (B[i])
+			B_max_digit++;
 
-		if (total[i] > MAX)
+		i++;
+	}
+
+	A_B_max_digit = A_max_digit + B_max_digit + 2;
+
+
+	// add total Line //
+	i = 0;
+
+	while ((A_max_digit>=0) || (B_max_digit>=0))
+	{
+		if (A_max_digit>=0)
+			char_to_IntAdd(total+A_max_digit--, A+i);
+		
+
+		if (B_max_digit>=0)
+			char_to_IntAdd(total+B_max_digit--, B+i);
+
+		i++;
+
+	}
+
+
+
+	// Compute total's Digit Line //
+	i = 0;
+
+	while (i<=A_B_max_digit)
+	{
+
+		while (total[i] >= MAX)
 		{
-			total[i] -= 10;
+			total[i] -= MAX;
 			total[i + 1] += 1;
-
-			//TestLine
-			//cout << total[i];
 		}
 
 		i++;
 	}
 
+
+	// Printing Line //
 	i--;
+	
+	bool print_start = false;
 
-	int j = 0;
-
-	while (j<=i)
+	while (i>=0)
 	{
-		cout << total[j++];
+		if (total[i])
+		{
+			cout << total[i--];
+			print_start = true;
+		}
+
+		else if (print_start == true)
+			cout << total[i--];
+
+		else
+			i--;
+
+
+		if ((i == 1) && (print_start == false))
+		{
+			print_start = true;
+			i--;
+		}
+
+
 	}
 
 
