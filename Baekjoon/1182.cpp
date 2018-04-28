6f35a706d875a65fa6901ddfcef0257780609832
ShootedBall <wjdtmdwls45@gmail.com>
2018-04-28 15:51:29 +0900

    [1182] I think structure of Tree.

--- Baekjoon/1182.cpp
+++ Baekjoon/1182.cpp
@@ -2,92 +2,60 @@
 #include<vector>
 using namespace std;
 int S;
+vector<int> number;
 
 inline int Check(int check){ return	( S == check ) ? 1 : 0; }
 
-
-int GetSubset(vector<int> &number,int left, int right)
+int GetSubset(int temp, int index)
 {
 
-	// Base
-	if (left >= right)
-		return Check(number[right]);
+	//Base 1 for time shrot
+	if (index == (number.size() - 1))
+		return Check(temp + number[index]);
 
+	//Base 2
+	else if (index > (number.size() - 1))
+		return 0;
 
 	int total = 0;
-	int temp = 0;
-	int mid = (right + left) / 2 ;
 
-	//Divide
-	total += (GetSubset(number, left, mid) + GetSubset(number, mid + 1, right));
-
-	// Compute Line
-	int left_last = mid;
-	int right_first = mid + 1;
-	int left_complete = 0;
-	int right_complete = 0;
-
-
-	//TestLine
-	cout << "---TEST----" << endl;
-	cout << "left " << left << "    right : " << right << endl;
-
-
-	//Connect left_last <--> right_first
-	if (left_last >= left)
+	for (int i = index; i <= (number.size()-1); i++)
 	{
-		temp += number[left_last--];
-	}
-
-
-	if (right_first <= right)
-	{
-		temp += number[right_first++];
-	}
-
-	if (((left_last+1) >= left) && ((right_first-1) <= right))
-		if (Check(temp))
+		// Search now level like this
+		//   (бр)
+		// бс(бр) .....
+		//   (бр)
+		if (Check(temp + number[i]))
 			total += 1;
 
-	int point_left = left_last;
-	int point_right = right_first;
+		// Search next level like this
+		//    (бр)
+		//  бс бр
+		//бсбр бр .......
+		//  бр бр
+		//     бр
 
-	// Search
-	while ((left_last >= left) || (right_first<= right))
-	{
-
-		while ((point_left >= left) || (point_right <= right))
+		//     бр
+		//  бс(бр)
+		//бсбр бр .......
+		//  бр бр
+		//     бр
+		for (int j = i+1; j <= (number.size() - 1); j++)
 		{
+			//Check temp + now_node + child_node
+			if (Check(temp + number[i] + number[j]))
+				total += 1;
 
-			// search (бр)бсбс
-			if (point_left >= left)
-			{
-				if (Check(temp + number[point_left--]))
-					total += 1;
-			}
-
-
-
-
-			// search бсбс(бр)
-			if (point_right <= right)
-			{
-				if (Check(temp + number[point_right++]))
-					total += 1;
-			}
-		}//End of nested while
+			// Call child_child_node
+			total += GetSubset(temp + number[i] + number[j], j+1);
+		}
 
 
 
-
-	}//End of while
-
-	//TestLine
-	cout << "Total Stack :" << total << endl;
+	}// End of for
 
 
 	return total;
-
 
 
 }
@@ -98,8 +66,6 @@ int main(void)
 {
 	
 	int N;
-	
-	vector<int> number;
 	int temp_number;
 
 	cin >> N;
@@ -111,10 +77,7 @@ int main(void)
 		number.push_back(temp_number);
 	}
 	
-
-
-
-	int val = GetSubset(number, 0, number.size() - 1);
+	int val = GetSubset(0, 0);
 
 	cout << val << '\n';
 
