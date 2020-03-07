2655237e6d5a52aff7aa0533f9e03060e9c7fc9c
LaVi <lastvirgo266@gmail.com>
2020-03-07 13:15:12 +0900

    Create 11021.cpp

---
+++ Baekjoon/11021.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main(){
+    int T;
+    int A,B;
+
+    scanf("%d", &T);
+    
+    for(int i=0; i<T; i++){
+        scanf("%d %d", &A,&B);
+        printf("Case #%d: %d\n",i+1, A+B);
+    }
+
+    return 0;
+
+}
