7349a2a94d8d9e2c14175074a3f3e3bc67406820
LaVi <lastvirgo266@gmail.com>
2020-03-17 12:46:12 +0900

    Update 17140_v1.cpp

--- Baekjoon/17140_v1.cpp
+++ Baekjoon/17140_v1.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 
 int map[100][100] = {0,};
-
+int y_len = 3;
+int x_len = 3;
 
 
 
@@ -13,11 +14,15 @@ int Change(int len){
     //arr[i][j] ( i==> COUNT, j==> NUMBER)
     vector< int, int > arr;
 
-    for(int i=0; i<len; i++){
-        for(int j=0; j<len; j++){
+    //Row Change
+    for(int i=0; i<y_len; i++){
+        for(int j=0; j<x_len; j++){
             
         }
     }
+
+
+
 }
 
 
