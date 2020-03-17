b8c75e26a0b548e9b0ba4bab3e61d94f03dba44c
LaVi <lastvirgo266@gmail.com>
2020-03-17 12:44:11 +0900

    Create 17140_v1.cpp

---
+++ Baekjoon/17140_v1.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<vector>
+using namespace std;
+
+
+int map[100][100] = {0,};
+
+
+
+
+int Change(int len){
+
+    //arr[i][j] ( i==> COUNT, j==> NUMBER)
+    vector< int, int > arr;
+
+    for(int i=0; i<len; i++){
+        for(int j=0; j<len; j++){
+            
+        }
+    }
+}
+
+
+
+
+int main(){
+
+
+
+
+}
