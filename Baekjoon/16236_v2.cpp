5bd8ad842322c71d0fcc7e3688b1481a318af342
LaVi <lastvirgo266@gmail.com>
2020-03-22 18:56:34 +0900

    update

--- Baekjoon/16236_v2.cpp
+++ Baekjoon/16236_v2.cpp
@@ -8,7 +8,19 @@ int shark_size=2;
 int shark_needs = 0;
 int fish[20][20] = {0,};
 
-int direction[4][2] = { {0, -1}, {-1, 0}, {1,0}, {0,1} };
+int move_direction[4][2] = { {-1, 0}, {0, -1}, {1,0}, {0,1} };
+int eat_direction[4][2] = { {0,-1}, {-1 ,0}, {0,1}, {1,0} };
+
+
+void Test(){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++)
+            printf("%d ",fish[i][j]);
+        printf("\n");
+    }
+
+    printf("\n\n\n");
+}
 
 
 //V2작성 30분 소모
@@ -38,8 +50,10 @@ int main(){
     queue<pair <int, int> > shark_q;
     shark_q.push({start_y, start_x});
     int last_second = 0;
+    shark_needs = shark_size;
 
     while(!shark_q.empty()){
+
         int visited[20][20] = {0, };
 
         pair< int, int > temp = shark_q.front();
@@ -54,13 +68,54 @@ int main(){
 
         fish_q.push( {{shark_y, shark_x}, last_second});
 
+        Test();
+
         while(!fish_q.empty()){
             pair<int, int> temp_pos = fish_q.front().first;
             int temp_second = fish_q.front().second;
 
+            fish_q.pop();
+
+        printf("%d %d\n",temp_second, shark_size);
+
+        bool escape_ok = false;
+
+            //Eat fish
             for(int i=0; i<4; i++){
-                int go_y = temp_pos.first + direction[i][0];
-                int go_x = temp_pos.second + direction[i][1];
+                int go_y = temp_pos.first + eat_direction[i][0];
+                int go_x = temp_pos.second + eat_direction[i][1];
+
+                if(0<= go_y && go_y < N &&
+                   0<= go_x && go_x < N &&
+                   visited[go_y][go_x] == 0 &&
+                   0 < fish[go_y][go_x] && 
+                   fish[go_y][go_x] < shark_size){
+                       visited[go_y][go_x] = 1;
+                       pair < pair < int, int > , int > temp;
+                       
+                        shark_needs--;
+                        if(shark_needs == 0){
+                            shark_size++;
+                            shark_needs = shark_size;
+                        }
+
+                        
+                        fish[go_y][go_x] = 0;
+                        shark_q.push({go_y, go_x});
+                        last_second = temp_second+1;
+                        escape_ok = true;
+                        break;
+
+                    }
+            }
+
+            if(escape_ok)
+                break;
+
+            //move shark
+            for(int i=0; i<4; i++){
+                int go_y = temp_pos.first + move_direction[i][0];
+                int go_x = temp_pos.second + move_direction[i][1];
 
                 if(0<= go_y && go_y <N &&
                    0<= go_x && go_x < N &&
@@ -68,22 +123,13 @@ int main(){
                    fish[go_y][go_x] <= shark_size){
                        visited[go_y][go_x] = 1;
                        pair < pair < int, int > , int > temp; 
+                       fish_q.push({ {go_y, go_x}, temp_second+1});
 
-                       if(shark_size > fish[go_y][go_x])
-                            fish_q.push({ {go_y, go_x}, temp_second+1});
-                        
-                        else{
-                            shark_size++;
-                            shark_q.push({go_y, go_x});
-                            last_second = temp_second;
-                            break;
-                        }
-                       
-                   }
+                    }
 
             }
 
-        }
+        }//End While
 
 
     }
