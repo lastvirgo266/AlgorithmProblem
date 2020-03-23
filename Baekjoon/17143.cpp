4e118697e65ee96703c365b8d96e1b5f195adef3
LaVi <lastvirgo266@gmail.com>
2020-03-23 11:21:36 +0900

    Update 17143.cpp

--- Baekjoon/17143.cpp
+++ Baekjoon/17143.cpp
@@ -11,10 +11,11 @@ typedef struct __shark{
 }Shark;
 
 int shark_map[101][101] = {0,};
+int fish = 0;
 vector< pair< pair<int, int> , Shark> > shark_list;
 int Y,X;
 
-//초안 40분까지 걸림 다시시작
+//초안 작성 1시간걸림
 //key, value ==>고유 값이 필요한데 pair를 써버림... (map을 써야했음)
 
 
@@ -41,7 +42,23 @@ void ParsingDirection(int direction, Shark &shark){
 
 }
 
-void EatShark();
+void EatShark(int row){
+    bool cascade_end = false;
+
+    for(int i=0; i<Y ; i++)
+        if(shark_map[i][row] == 1){
+            for(int k=0; k<shark_list.size(); k++){
+                if(shark_list[k].first.first == i &&
+                   shark_list[k].first.second == row){
+                       fish += shark_list[k].second.size;
+                       shark_list.erase(shark_list.begin()+k);
+                       shark_map[i][row] = 0;
+                       return;
+                   }
+            }
+        }
+
+}
 
 void MoveAllShark(){
 
@@ -81,52 +98,74 @@ void MoveAllShark(){
 
            }
 
-           new_map[y][x] = 1;
-
-            if (new_shark_list.find("f") == m.end() ) {
-            // not found
-            } else {
-            // found
-            }
-
-           new_shark_list.push_back({ {y,x}, shark } );
-
-
         }//End if
 
         //Move shark X-axis
-        if(shark.direction[0] != 0){
+        if(shark.direction[1] != 0){
            int can_distance = shark.speed;
            while(can_distance != 0){
            
-                if( 0 > y + can_distance*shark.direction[0]){
-                    int reminder = y + can_distance*shark.direction[0];
+                if( 0 > x + can_distance*shark.direction[1]){
+                    int reminder = x + can_distance*shark.direction[1];
                     can_distance = (-reminder);
-                    shark.direction[0] = (-shark.direction[0]);
-                    y = 0;
+                    shark.direction[1] = (-shark.direction[1]);
+                    x = 0;
                 }
 
-                if(Y <= y + can_distance*shark.direction[0]){
-                    int reminder = y - can_distance*shark.direction[0];
+                if(X <= X + can_distance*shark.direction[1]){
+                    int reminder = x - can_distance*shark.direction[1];
                     can_distance = (-reminder);
-                    shark.direction[0] = (-shark.direction[0]);
-                    y = Y-1; 
+                    shark.direction[1] = (-shark.direction[1]);
+                    x = X-1; 
                 }
 
                 else{
-                    y = y + can_distance*shark.direction[0];
+                    x = x + can_distance*shark.direction[1];
                     can_distance = 0;
                 }
 
            }
 
+
+
         }//End if
+
+
+           if(new_map[y][x] != 0){
+               for(int i=0; i<new_shark_list.size(); i++){
+                   if(new_shark_list[i].first.first == y &&
+                      new_shark_list[i].first.second == x &&
+                      new_shark_list[i].second.size < shark.size){
+                        new_shark_list[i].second = shark;
+                      }
+               }
+           }
+
+            else{
+                new_shark_list.push_back({ {y,x}, shark } );
+            }
 
     }
 
 
+    //Reset
+
+    shark_list.clear();
+
+    for(int i=0; i<new_shark_list.size(); i++){
+        shark_list.push_back(new_shark_list[i]);
+    }
+
+    //Rest
+    for(int i=0; i<Y; i++)
+        for(int j=0; j<X; j++)
+            shark_map[i][j] = new_map[i][j];
+
+
 
 }
+
+
 
 int main(){
 
@@ -148,6 +187,17 @@ int main(){
 
 
     //초기화 종료
+
+    for(int i=0; i<Y; i++){
+        EatShark(i);
+        MoveAllShark();
+    }
+
+
+    printf("%d",fish);
+
+
+    return 0;
 
 
 }
