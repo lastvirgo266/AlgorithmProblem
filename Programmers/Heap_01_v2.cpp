f830e273130961be44fdfe568cf5bafc031abf3b
LaVi <lastvirgo266@gmail.com>
2020-04-09 11:44:03 +0900

    Update

---
+++ Programmers/Heap_01_v2.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    
+    sort(scoville.begin(), scoville.end());
+    
+    while(scoville.size() != 0){
+        
+        bool check = false;
+        
+        if(scoville[0] < K){
+            check = true;
+        }
+        
+        if(check){
+            
+            if(scoville.size() <= 1)
+                return -1;
+            
+            int first = scoville[0];
+            scoville.erase(scoville.begin());
+            int second = scoville[0];
+            scoville.erase(scoville.begin());
+            
+            int result = first + (second *2);
+            answer++;
+
+            for(int i=0; i<scoville.size(); i++){
+                if(result <= scoville[i]){
+                    scoville.insert(scoville.begin()+i, result);
+                    result = -1;
+                    break;
+                }
+            }
+            
+            //Exception
+            if(result != -1) //전부다 result 보다 작으면 못들어가는 경우의 수를 생각해줬음 근데 시간초과 걸림
+                scoville.push_back(result);
+            
+        }
+        
+        else{
+            return answer;
+        }
+    
+    }
+    
+    
+    return answer;
+}
