9ea700f4f845a77fc84af47705f37b5f7542d2a9
LaVi <lastvirgo266@naver.com>
2020-07-15 23:28:24 +0900

    [프로그래머스] 조이스틱 1차 풀이

---
+++ Programmers/조이스틱.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int[] visited = new int[name.length()];
+        
+        for(int i=0; i<name.length(); i++){
+            visited[i] = 0;
+        }
+        
+        
+        for(int i = 0; i<name.length(); i++){
+            
+            
+            
+        }
+        
+        
+        
+        return answer;
+    }
+}
